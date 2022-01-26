ef2ce34f86616e494790528bed1ec24a3612ea52
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-01-26 14:15:52 -0300

    mongoose e database

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,5 @@
 export const config = {
-  API_URL: "http://localhost:5000",
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://teste:<teste>@cluster0.0gglr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
