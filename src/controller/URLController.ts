e224ac98493bdf3a1e94993d28def902e01c8660
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2022-01-26 16:57:54 -0300

    refatoração

--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
-import shortId from "shortid";
-import { config } from "../config/Constants";
+import { Request, Response } from 'express'
+import shortId from 'shortid'
+import { config } from '../config/Constants'
 import { URLModel } from '../database/model/URL'
 
 export class URLController {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from "express";
-import { URLController } from "./controller/URLController";
-import { MongoConnection } from "./database/MongoConnection";
+import express from 'express'
+import { URLController } from './controller/URLController'
+import { MongoConnection } from './database/MongoConnection'
 
-const api = express();
+const api = express()
 api.use(express.json())
 
 const database = new MongoConnection()
-database.connect();
+database.connect()
 
-const urlController = new URLController();
+const urlController = new URLController()
 api.post('/shorten', urlController.shorten)
 api.get('/:hash', urlController.redirect)
 
-api.listen(5000, () => console.log('listening on port 5000'));
+api.listen(5000, () => console.log('listening on port 5000'))
